1ebffedbc39e8167a652d1e2965db8fa0e8c65b5
Travis French <travisfrench@Traviss-MacBook-Pro.local>
2014-11-24 14:35:36 -0600

    Insured input of numeric value on high and low.

--- highlow_game.php
+++ highlow_game.php
@@ -1,39 +1,58 @@
 <?php
 
-$luckynumber = mt_rand(1, 100);
 $count = 0;
 
-if($argc == 3){
-	$luckynumber = mt_rand($argv[1], $argv[2]);
-}
-else {$luckynumber = $luckynumber;
+if ($argc == 3){
+
+	if (is_numeric($argv[1])){
+		$low = $argv[1];
+	}
+		else {
+			echo "Please input numeric value for low number\n";
+			$low = trim(fgets(STDIN));
+			echo "\n";
+		}
+	if (is_numeric($argv[2])){
+		$high = $argv[2];
+	}
+		else {
+			echo "Please input numeric value for high number\n";
+			$high = trim(fgets(STDIN));
+			echo "\n";
+		}
+	
+} 
+else {
+	$low = 1;
+	$high = 100;
 
 }
+$luckynumber = mt_rand($low, $high);
 
-do{	
+do {	
 
 	$count++;
 
-	fwrite(STDOUT, 'Guess a Number!' . PHP_EOL);
+	fwrite (STDOUT, 'Guess a Number!' . PHP_EOL);
 
 		$number = trim(fgets(STDIN));
-		if(is_numeric($number)){
+		if (is_numeric($number)){
 
-			if($number == $luckynumber){
+			if ($number == $luckynumber){
 				echo 'Congradulations, You Won!' . PHP_EOL;
 			} 
-			elseif($number < $luckynumber){
+			elseif ($number < $luckynumber){
 				fwrite(STDOUT, 'Higher' . PHP_EOL);
 			}
 			else {
 				fwrite(STDOUT, 'Lower' . PHP_EOL);
 			}
 		}
-		else{
+		else {
 			fwrite(STDOUT, "Sorry, Please input numeric value\n");
 		}
 
-} while($number != $luckynumber);
+} while ($number != $luckynumber);
 
 echo "It took you $count tries to get it correct\n";
 
